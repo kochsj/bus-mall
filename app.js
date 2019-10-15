0ff924e911667624ce824262f28bb9b08cecd3c0
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-10-15 14:29:12 -0700

    created second dataset for each label inside of the chart. need to un-hardcode chart.

--- app.js
+++ app.js
@@ -170,39 +170,32 @@ var numberOfPics = 0;
 
 //MAKING A BAR GRAPH////////////////////////////
 // <!-- from https://www.chartjs.org/docs/latest/ -->
-// var ctx = document.getElementById('myGraph').getContext('2d');
-// var myChart = new Chart(ctx, {
-//   type: 'bar',
-//   data: {
-//     labels: ['Banana', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//     datasets: [{
-//       label: '# of Votes',
-//       data: [12, 19, 3, 5, 2, 3],
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(54, 162, 235, 0.2)',
-//         'rgba(255, 206, 86, 0.2)',
-//         'rgba(75, 192, 192, 0.2)',
-//         'rgba(153, 102, 255, 0.2)',
-//         'rgba(255, 159, 64, 0.2)'
-//       ],
-//       borderColor: [
-//         'rgba(255, 99, 132, 1)',
-//         'rgba(54, 162, 235, 1)',
-//         'rgba(255, 206, 86, 1)',
-//         'rgba(75, 192, 192, 1)',
-//         'rgba(153, 102, 255, 1)',
-//         'rgba(255, 159, 64, 1)'
-//       ],
-//       borderWidth: 5
-//     }]
-//   },
-//   options: {
-//     scales: {
-//       yAxes: [{
-//         ticks: {
-//           beginAtZero: true
-//         }
-//       }]
-//     }
-//   }
+var ctx = document.getElementById('myGraph').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Banana', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Mangosteen'],
+    datasets: [{
+      label: '# of Views',
+      data: [12, 19, 3, 5, 2, 3, 10],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 5,
+    }, {
+      label: '# of Clicks',
+      data: [3, 0, 4, 2, 5, 1, 10],
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      borderColor: 'rgba(75, 192, 192, 0.2)',
+      borderWidth: 5,
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
